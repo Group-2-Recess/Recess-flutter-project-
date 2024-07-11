de07320f55c02d381c0a3c32ebfb5eee3d4b6e8e
fredrick01 <kigongobazirafred@gmail.com>
2024-07-11 17:11:41 +0300

    changed the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:medical_reminder/Components/Screens/forgotPassword.dart';
 import 'package:medical_reminder/Components/Screens/login.dart';
 import 'package:medical_reminder/Components/Screens/signup.dart';
-
+import 'package:medical_reminder/selection_page.dart'; // Adjust this import
+import 'package:medical_reminder/patient_page.dart';    // Adjust this import
+import 'package:medical_reminder/caregiver.dart';    // Import CaregiverPage
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: "AIzaSyCMeU0NysPEF_zf09VXRCKifZWOMNhhc1U",
-      appId: "1:564875037088:web:fed48c50bb12f7882abcc5",
-      messagingSenderId: "564875037088",
-      projectId: "medical-reminder-app-8ce9e",
-      storageBucket: "medical-reminder-app-8ce9e.appspot.com",
+      apiKey: "your_api_key",
+      appId: "your_app_id",
+      messagingSenderId: "your_sender_id",
+      projectId: "your_project_id",
+      storageBucket: "your_storage_bucket",
     ),
   );
   runApp(const MyApp());
@@ -31,6 +33,9 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const Login(),
         '/signup': (context) => const Signup(),
         '/forgot-password': (context) => const ForgotPassword(),
+        '/patient': (context) => const PatientPage(),
+        '/selection': (context) => const SelectionPage(),
+        '/caregiver': (context) => const CaregiverPage(),  // Add route for CaregiverPage
       },
     );
   }
